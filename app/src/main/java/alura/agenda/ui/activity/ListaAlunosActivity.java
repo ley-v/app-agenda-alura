24ef1a01bc59a8cfc66075178ff79f184a82c65a
ley <ley.dgv@gmail.com>
2021-06-17 21:26:11 +0100

    Adicionando layout do formulário

--- app/src/main/java/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,29 +1,33 @@
-package alura.agenda;
+package alura.agenda.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class MainActivity extends Activity {
+import alura.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int activity_main = R.layout.activity_main;
+        setTitle("Lista de alunos");
+
+        int activity_main = R.layout.activity_lista_alunos;
         Toast.makeText(this, String.valueOf(activity_main), Toast.LENGTH_LONG).show();
         setContentView(activity_main);
 
         List<String> alunos = new LinkedList<>(Arrays.asList("Milly", "Joabson", "Hazel", "brit"));
 
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
     }
 
